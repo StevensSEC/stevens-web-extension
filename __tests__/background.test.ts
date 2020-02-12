4f1a49de4387b56da2af19d9d2aff9eec3c1274e
adapap <19696846+adapap@users.noreply.github.com>
2020-02-12 11:36:01 -0500

    I don't know how to fix this

--- __tests__/background.test.ts
+++ __tests__/background.test.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import '../src/background.ts';
-=======
-// import '../src/background.ts';
->>>>>>> Fix merge conflicts, disable test
 
 describe('Chrome extension startup', () => {
     it('should start correctly', () => {
