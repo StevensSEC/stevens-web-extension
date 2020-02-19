ab259fd92cb177efc27ab2c14a82a32b4b4fdbb7
adapap <19696846+adapap@users.noreply.github.com>
2020-02-18 20:14:51 -0500

    Template for improving options page

--- src/options.ts
+++ src/options.ts
@@ -2,6 +2,17 @@ import log from './shared/utility';
 import './options.pug';
 
 document.addEventListener('DOMContentLoaded', e => {
-    let options: Element = document.querySelector('#active-features');
-    log(options);
+    let activeFeatures = {};
+    document
+        .querySelectorAll('#active-features > input[name=features]')
+        .forEach((x: HTMLInputElement) => {
+            activeFeatures[x.id] = x.checked;
+        });
+    log(activeFeatures);
+    /* To Do
+    - Update the activeFeatures after someone (un)checks a feature
+    - Save/retrieve the activeFeatures from chrome local storage
+    Here is the first line to get started on the local storage
+      chrome.storage.local.get('activeFeatures', result => { ... })
+    */
 });
